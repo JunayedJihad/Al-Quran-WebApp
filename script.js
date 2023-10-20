8b3648657bebfbeedfe11524e3a7926ea5bd8534
Junayed Jihad <junayedjihad48@gmail.com>
2023-10-20 18:34:48 +0600

    Final Commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let container = document.getElementById("container");
-//  let ayatInfo = "";
+
  let audioAyats = [];
 let ayatNo = [];
+
+document.getElementById('close').addEventListener("click", () => {
+    document.getElementById("warning").remove()
+})
 
 function stopAudio() {
     document.getElementById("audio").pause();
@@ -10,69 +14,78 @@ function stopAudio() {
 function bringModal(number) {
         document.getElementById('staticBackdropLabel').innerHTML = `Ayat no ${number}`
         document.getElementById('audio').src=audioAyats[number-1]
-    }
+}
 
 async function bringSura(no) {
     let ayatInfo = "";
-    // let audioAyats = [];
-    // let ayatNo = [];
+
+    let suraLink = `https://api.quran.gading.dev/surah/${no}`;
+    let suraResponse = await fetch(suraLink);
+      suraResponse = await suraResponse.json();
 
 
 
+      let suraInfo = `
+       <div class="card suraInfo" ondblclick="bringSuraList()">
+              <h4>${suraResponse.data.name.long}</h4>
+              <h5>${suraResponse.data.name.transliteration.en}</h5>
+              <h6>${suraResponse.data.name.translation.en}</h6>
+              <div class="ayat-source">
+
+                  <span>Ayat-${suraResponse.data.numberOfVerses}</span>
+                  <span>${suraResponse.data.revelation.id}</span>
+              </div>
+
+          </div>
+
+      `;
+
+      suraResponse.data.verses.forEach((verse) => {
+
+          audioAyats.push(verse.audio.primary)
+          ayatNo.push(verse.number.inSurah);
+
+            if (verse.meta.sajda.recommended || verse.meta.sajda.obligatory) {
+                let sajda = "Sajda";
+
+                 ayatInfo += `
+              <div class="card suraCard" ondblclick="bringSuraList()">
+              <div class="ayat">
+                  <span>${verse.number.inSurah}</span>
+                  <span title="Play" onclick="bringModal(${verse.number.inSurah})" class="icon" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="fa-solid fa-circle-play"></i></span>
+              </div>
+              <div class="arabic">${verse.text.arab}
+              <span class="sajda p-1 text-bg-warning">${sajda}</span>
+              </div>
+              <div class="transliteration">${verse.text.transliteration.en}</div>
+              <div class="meaning">${verse.translation.en}</div>
+          </div>
+
+          `;
+
+          }
+            else {
+                 ayatInfo += `
+              <div class="card suraCard" ondblclick="bringSuraList()">
+              <div class="ayat">
+                  <span>${verse.number.inSurah}</span>
+                  <span title="Play" onclick="bringModal(${verse.number.inSurah})" class="icon" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="fa-solid fa-circle-play"></i></span>
+              </div>
+              <div class="arabic">${verse.text.arab}
+              </div>
+              <div class="transliteration">${verse.text.transliteration.en}</div>
+              <div class="meaning">${verse.translation.en}</div>
+          </div>
+
+          `;
+          }
 
 
-  let suraLink = `https://api.quran.gading.dev/surah/${no}`;
-  let suraResponse = await fetch(suraLink);
-    suraResponse = await suraResponse.json();
-
-
-
-    let suraInfo = `
-     <div class="card suraInfo" ondblclick="bringSuraList()">
-            <h4>${suraResponse.data.name.long}</h4>
-            <h5>${suraResponse.data.name.transliteration.en}</h5>
-            <h6>${suraResponse.data.name.translation.en}</h6>
-            <div class="ayat-source">
-
-                <span>Ayat-${suraResponse.data.numberOfVerses}</span>
-                <span>${suraResponse.data.revelation.id}</span>
-            </div>
-
-        </div>
-
-    `;
-
-    suraResponse.data.verses.forEach((verse) => {
-
-        audioAyats.push(verse.audio.primary)
-        ayatNo.push(verse.number.inSurah);
-
-        ayatInfo += `
-            <div class="card suraCard" ondblclick="bringSuraList()">
-            <div class="ayat">
-                <span>${verse.number.inSurah}</span>
-                <span onclick="bringModal(${verse.number.inSurah})" class="icon" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="fa-solid fa-circle-play"></i></span>
-            </div>
-            <div class="arabic">${verse.text.arab}<span id="sejdah"></span>
-            </div>
-            <div class="transliteration">${verse.text.transliteration.en}</div>
-            <div class="meaning">${verse.translation.en}</div>
-        </div>
-
+        })
+        container.innerHTML = `
+        ${suraInfo}
+        ${ayatInfo}
         `;
-
-
-
-
-
-        console.log(ayatNo);
-
-
-    })
-    container.innerHTML = `
-    ${suraInfo}
-    ${ayatInfo}
-    `;
 
 
 
@@ -104,7 +117,8 @@ let bringSuraList= async () => {
 
     container.innerHTML = suraList;
   } catch {
-    throw new Error("Something went wrong!");
+      alert('Something went wrong')
+      throw new Error("Something went wrong!");
   }
 }
 bringSuraList()
